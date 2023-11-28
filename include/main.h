db8712a25025b3de254e694b79bfb88eb94fc36a
admin <admin@DESKTOP-16V9A30>
2023-11-29 04:32:47 +0800

    添加项目文件。

---
+++ include/main.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_dx11.h"
+#include "Font.h"
+#include <d3d11.h>
+#include <stdio.h>
+
+
+#pragma comment(lib,"d3d11.lib")
+#pragma execution_character_set("utf-8")
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+
+ImVec4 RGBAtoIV4(int r, int g, int b, int a) {
+    float newr = r / 255.0f;
+    float newg = g / 255.0f;
+    float newb = b / 255.0f;
+    float newa = a / 255.0f;
+    return ImVec4(newr, newg, newb, newa);
+}
